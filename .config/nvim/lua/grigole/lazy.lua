e905e303bf12c595aa170169398d0cbdfcc54410
Greg Rigole <grigole@gvrsoft.com>
2024-03-08 20:39:49 -0500

    Minor changes.

--- .config/nvim/lua/grigole/lazy.lua
+++ .config/nvim/lua/grigole/lazy.lua
@@ -23,6 +23,8 @@ require('lazy').setup {
 		config = function()
 		end,
 	},
+	{ 'vim-airline/vim-airline-themes' },
+
 	{ 'j-hui/fidget.nvim' },
 
 	-- See `:help gitsigns` to understand what the configuration keys do
@@ -45,6 +47,7 @@ require('lazy').setup {
 	{ "mbbill/undotree" },
 	{ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
 	{ 'nvim-treesitter/playground' },
+	{ 'tpope/vim-fugitive' },
 
 	{
 		'nvim-telescope/telescope.nvim',
@@ -54,22 +57,22 @@ require('lazy').setup {
 		{ -- If encountering errors, see telescope-fzf-native README for install instructions
 			'nvim-telescope/telescope-fzf-native.nvim',
 
-		-- `build` is used to run some command when the plugin is installed/updated.
-		-- This is only run then, not every time Neovim starts up.
-		build = 'make',
+			-- `build` is used to run some command when the plugin is installed/updated.
+			-- This is only run then, not every time Neovim starts up.
+			build = 'make',
 
-		-- `cond` is a condition used to determine whether this plugin should be
-		-- installed and loaded.
-		cond = function()
-			return vim.fn.executable 'make' == 1
-		end,
-	},
-	{ 'nvim-telescope/telescope-ui-select.nvim' },
+			-- `cond` is a condition used to determine whether this plugin should be
+			-- installed and loaded.
+			cond = function()
+				return vim.fn.executable 'make' == 1
+			end,
+		},
+		{ 'nvim-telescope/telescope-ui-select.nvim' },
 
-	-- Useful for getting pretty icons, but requires special font.
-	--  If you already have a Nerd Font, or terminal set up with fallback fonts
-	--  you can enable this
-	-- { 'nvim-tree/nvim-web-devicons' }
+		-- Useful for getting pretty icons, but requires special font.
+		--  If you already have a Nerd Font, or terminal set up with fallback fonts
+		--  you can enable this
+		{ 'nvim-tree/nvim-web-devicons' }
 	},
 
 	{ "ThePrimeagen/harpoon",
@@ -89,7 +92,7 @@ require('lazy').setup {
 		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
 		{ 'j-hui/fidget.nvim', opts = {} },
 		},
-	    config = function()
+		config = function()
 			-- Brief Aside: **What is LSP?**
 			--
 			-- LSP is an acronym you've probably heard, but might not understand what it is.
@@ -245,7 +248,7 @@ require('lazy').setup {
 								callSnippet = 'Replace',
 							},
 							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-							-- diagnostics = { disable = { 'missing-fields' } },
+							diagnostics = { disable = { 'missing-fields' } },
 						},
 					},
 				},
