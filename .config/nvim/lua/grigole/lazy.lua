26c2cf25a59a49a56ed895764f2b0772e4cddeda
Greg Rigole <grigole@gvrsoft.com>
2024-05-10 18:08:50 -0400

    Removed old packer.lua.
    Added airline symbols,

--- .config/nvim/lua/grigole/lazy.lua
+++ .config/nvim/lua/grigole/lazy.lua
@@ -21,6 +21,16 @@ require('lazy').setup {
 	opts = {},
 	-- with Packer it didn't need a config funtion. Go figure...
 	config = function()
+		if not vim.g.airline_symbols then
+			vim.g.airline_symbols = {}
+		end
+
+		-- vim-powerline symbols
+		vim.g.airline_powerline_symbols = 1
+		vim.g.airline_left_sep = ''
+		vim.g.airline_left_alt_sep = ''
+		vim.g.airline_right_sep = ''
+		vim.g.airline_right_alt_sep = ''
 	end,
 },
 { 'vim-airline/vim-airline-themes' },
