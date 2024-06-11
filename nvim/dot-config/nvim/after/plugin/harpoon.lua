eac2b3717d48d0258acbe8a4fc72185102124324
Greg Rigole <grigole@gvrsoft.com>
2024-06-11 17:02:45 -0400

    Removed mini.status line, as I didn't realize it was even there and it conflicted with vim-airline.
    Remapped four keymaps for Harpoon, as they conflicted with
    some of TJ's stuff.

--- nvim/dot-config/nvim/after/plugin/harpoon.lua
+++ nvim/dot-config/nvim/after/plugin/harpoon.lua
@@ -2,14 +2,14 @@ local harpoon = require("harpoon")
 
 harpoon:setup()
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 -- These conflict with Teej's Telescope mappings!
--- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
--- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
--- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
--- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Telescope[1]" } )
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Telescope[2]" } )
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Telescope[3]" } )
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Telescope[4]" } )
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
