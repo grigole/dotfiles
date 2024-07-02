2c8057a64e89902794f2aeba6de5b9d8a7d4c921
Greg Rigole <grigole@gvrsoft.com>
2024-07-02 11:12:43 -0400

    Initial set up of neotest with nvim-plenary as the test harness.

---
+++ nvim/dot-config/nvim/lua/grigole/plugins/neotest.lua
@@ -0,0 +1,13 @@
+return {
+
+	{ "nvim-neotest/neotest",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"nvim-neotest/neotest-plenary",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter"
+	},
+	},
+
+}
